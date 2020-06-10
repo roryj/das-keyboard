9dd4be83892e579c69b078217dc9a0ce90f913ab
Rory Jacob <roryjacob@gmail.com>
2020-06-09 23:51:16 -0700

    Remove rate limiter from calling code, use clear all signals

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/roryj/das-keyboard/client"
 	"github.com/roryj/das-keyboard/images"
 	"github.com/roryj/das-keyboard/keyboard"
-	"go.uber.org/ratelimit"
 )
 
 func main() {
@@ -18,26 +17,16 @@ func main() {
 }
 
 func makeCanadaFlag(c client.Client, img images.Image) {
-	rate := ratelimit.New(10)
-	var signals []client.SignalResponse
 	for y, row := range img {
 		for x, colour := range row {
-			rate.Take()
-			z := keyboard.NewXYZone(uint(x), uint(y))
-			r, err := c.CreateSignal(z, keyboard.BREATHE, colour)
+			z := keyboard.NewXYZone(uint(x)+1, uint(y))
+			_, err := c.CreateSignal(z, keyboard.BREATHE, colour)
 			if err != nil {
 				fmt.Printf("err: %v\n", err)
 			}
-			signals = append(signals, r)
 		}
 	}
 
 	time.Sleep(10 * time.Second)
-	for _, signal := range signals {
-		rate.Take()
-		err := c.DeleteSignal(signal.Id)
-		if err != nil {
-			fmt.Printf("err: %v", err)
-		}
-	}
+	c.ClearAllSignals()
 }
