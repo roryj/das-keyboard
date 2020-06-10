9d39d7711f57c1f19bc36a2cfbdcb99a2fff1fb2
Rory Jacob <roryjacob@gmail.com>
2020-06-09 23:15:35 -0700

    Remove unused comments

--- cmd/main.go
+++ cmd/main.go
@@ -15,24 +15,6 @@ func main() {
 	c := client.NewKeyboardClient(27301)
 
 	makeCanadaFlag(c, images.CANADA_FLAG)
-
-	// zone := keyboard.NewKeyZone("Q")
-	// r, err := c.CreateSignal(zone, keyboard.BLINK, colour.RED)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// zone2 := keyboard.NewKeyZone("T")
-	// _, err = c.GetSignal(zone2)
-	// if err != nil {
-	// 	fmt.Printf("err: %v\n", err)
-	// }
-
-	// time.Sleep(10 * time.Second)
-	// err = c.DeleteSignal(r.Id)
-	// if err != nil {
-	// 	panic(err)
-	// }
 }
 
 func makeCanadaFlag(c client.Client, img images.Image) {
