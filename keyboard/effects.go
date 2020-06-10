4f06eac01e2c822b82175d74733233fde8366b82
Rory Jacob <roryjacob@gmail.com>
2020-06-09 23:09:44 -0700

    Fix breathe constant

--- keyboard/effects.go
+++ keyboard/effects.go
@@ -5,6 +5,6 @@ type KeyEffect string
 const (
 	SET_COLOUR   KeyEffect = "SET_COLOR"
 	BLINK        KeyEffect = "BLINK"
-	SET_COLOR    KeyEffect = "BREATHE"
+	BREATHE      KeyEffect = "BREATHE"
 	COLOUR_CYCLE KeyEffect = "COLOR_CYCLE"
 )
