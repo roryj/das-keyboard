0f8f093b183393b1cb4c271a81035401643cc27a
Rory Jacob <roryjacob@gmail.com>
2020-06-12 01:01:54 -0700

    Add better validation checking on files
    This commit adds checking to the input file to ensure that it has the
    correct amount of rows as well as the correct amount of columns

--- editor/parser/parser.go
+++ editor/parser/parser.go
@@ -9,6 +9,9 @@ import (
 	"github.com/roryj/das-keyboard/images"
 )
 
+const keyboardRows = 6
+const keyboardColumns = 24
+
 func Parse(input []byte) (images.Image, error) {
 	log.Println("processed input from file")
 	text := string(input)
@@ -16,15 +19,18 @@ func Parse(input []byte) (images.Image, error) {
 	// now we take that and turn it into an image
 	final_image := images.Image{}
 
-	for _, line := range strings.Split(text, "\n") {
+	for i, line := range strings.Split(text, "\n") {
 		if line == "" {
 			break
 		}
 
-		row := make([]colour.Hex, 24)
+		row := make([]colour.Hex, keyboardColumns)
 
 		// tokens are in the format |<colour>|
-		// tokens := strings.Split(line, " ")
+		tokens := strings.Split(line, " ")
+		if len(tokens) != keyboardColumns {
+			return images.Image{}, fmt.Errorf("invalid number of columns in line %d of the file. Expected %d, got %d", i, keyboardColumns, len(tokens))
+		}
 		// log.Printf("tokens: %v\n", tokens)
 		for i, t := range strings.Split(line, " ") {
 			colour_string := strings.Replace(t, "|", "", -1)
@@ -37,6 +43,10 @@ func Parse(input []byte) (images.Image, error) {
 			row[i] = c
 		}
 		final_image = append(final_image, row)
+	}
+
+	if len(final_image) != 6 {
+		return images.Image{}, fmt.Errorf("ended up with an invalid number of rows. Expected %d, got %d", keyboardRows, len(final_image))
 	}
 
 	return final_image, nil
