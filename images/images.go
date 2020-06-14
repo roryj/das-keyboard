158d914735f65ebaab6c234082bf39df572fe5e6
Rory Jacob <roryjacob@gmail.com>
2020-06-13 19:25:10 -0700

    Add StealXY method to KeyboardImage

--- images/images.go
+++ images/images.go
@@ -8,6 +8,10 @@ func (k *KeyboardImage) CompareXY(x int, y int, other *KeyboardImage) bool {
 	return (*k)[x][y] == (*other)[x][y]
 }
 
+func (k *KeyboardImage) StealXY(x int, y int, other *KeyboardImage) {
+	(*k)[x][y] = (*other)[x][y]
+}
+
 var CANADA_FLAG KeyboardImage = KeyboardImage{
 	{
 		colour.RED, colour.RED, colour.RED, colour.RED, colour.RED, colour.WHITE, colour.WHITE, colour.WHITE, colour.WHITE, colour.WHITE, colour.RED, colour.RED, colour.WHITE, colour.WHITE, colour.WHITE, colour.WHITE, colour.WHITE, colour.RED, colour.RED, colour.RED, colour.RED, colour.RED, colour.RED,
