00b580cc1b6d917ed0560a938d81f4245d0fac5f
Rory Jacob <roryjacob@gmail.com>
2020-06-14 19:41:35 -0700

    Add String method to images for easier debugging!

--- images/images.go
+++ images/images.go
@@ -1,15 +1,41 @@
 package images
 
-import "github.com/roryj/das-keyboard/colour"
+import (
+	"fmt"
+
+	"github.com/roryj/das-keyboard/colour"
+)
 
 type KeyboardImage [][]colour.Hex
 
-func (k *KeyboardImage) CompareXY(x int, y int, other *KeyboardImage) bool {
-	return (*k)[x][y] == (*other)[x][y]
+func Copy(img KeyboardImage) KeyboardImage {
+	width := len(img[0])
+
+	var result KeyboardImage
+
+	for _, row := range img {
+		newRow := make([]colour.Hex, width)
+		copy(newRow, row)
+		result = append(result, newRow)
+	}
+
+	return result
 }
 
 func (k *KeyboardImage) StealXY(x int, y int, other *KeyboardImage) {
 	(*k)[x][y] = (*other)[x][y]
+
+func (k *KeyboardImage) String() string {
+	var result string
+	for y, row := range *k {
+		result += fmt.Sprintf("%d) ", y)
+		for _, colourValue := range row {
+			result += fmt.Sprintf("%s, ", colourValue.Hex())
+		}
+		result += "\n"
+	}
+	return result
+}
 
 var CLEAR_KEYBOARD KeyboardImage = KeyboardImage{
 	{
