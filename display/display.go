445edf712e202bae3e86eba44739fc635ceb4ba5
Rory Jacob <roryjacob@gmail.com>
2020-06-14 19:43:28 -0700

    Get basic Display implementation working
    This adds the basic ability to create a running display loop, and send
    updates asynchronously to get drawn on the next display update loop.
    Right now this uses a whole bunch of struct passing, and this is due to
    the many pointer issues I ran into during my original implementation.
    This can/should be changed later, but for now this works and therefore
    is ideal

--- display/display.go
+++ display/display.go
@@ -3,6 +3,7 @@ package display
 import (
 	"log"
 
+	"github.com/roryj/das-keyboard/colour"
 	"github.com/roryj/das-keyboard/images"
 	"github.com/roryj/das-keyboard/keyboard"
 	"go.uber.org/ratelimit"
@@ -10,25 +11,33 @@ import (
 
 type Display struct {
 	client           keyboard.Client
-	inputBuffer      *images.KeyboardImage
-	currentBuffer    *images.KeyboardImage
-	updateChannel    chan (*images.KeyboardImage)
+	inputBuffer      images.KeyboardImage
+	currentBuffer    images.KeyboardImage
+	updateChannel    chan (images.KeyboardImage)
 	keyUpdateLimiter ratelimit.Limiter
 	refreshLimiter   ratelimit.Limiter
 }
 
 func NewDisplay(client keyboard.Client) *Display {
-	keyUpdateLimiter := ratelimit.New(50)
-	refreshLimiter := 10
+	keyUpdateLimiter := ratelimit.New(10)
+	refreshLimiter := ratelimit.New(10)
+
+	// og := images.CLEAR_KEYBOARD
 
 	return &Display{
-		client: client,
-		inputBuffer: ,
+		client:           client,
+		inputBuffer:      images.Copy(images.CLEAR_KEYBOARD),
+		currentBuffer:    images.Copy(images.CLEAR_KEYBOARD),
+		updateChannel:    make(chan images.KeyboardImage),
+		keyUpdateLimiter: keyUpdateLimiter,
+		refreshLimiter:   refreshLimiter,
 	}
 }
 
-func (d *Display) Set(input *images.KeyboardImage) {
+func (d *Display) Set(input images.KeyboardImage) {
+	log.Printf("recieved input!")
 	d.updateChannel <- input
+	log.Printf("image data to channel")
 }
 
 func (d *Display) Start() {
@@ -39,19 +48,35 @@ func (d *Display) Start() {
 
 	// loop forever updating the display
 	for {
-		for y, row := range *d.currentBuffer {
-			for x, colour := range row {
-				if d.currentBuffer.CompareXY(x, y, d.inputBuffer) {
-					d.keyUpdateLimiter.Take()
-					log.Printf("update needed for %d,%d", x, y)
-					z := keyboard.NewXYZone(uint(x)+1, uint(y))
-					_, err := d.client.CreateSignal(z, keyboard.SET_COLOUR, colour)
-					if err != nil {
-						log.Fatalf("failed to update signal: %v", err)
-					}
-					d.currentBuffer.StealXY(x, y, d.inputBuffer)
+		// log.Printf("display buffer: %s", d.currentBuffer.String())
+		// log.Printf("input buffer: %s", d.inputBuffer.String())
+
+		for rowIndex, row := range d.currentBuffer {
+			// log.Printf("messing with row %d: %v", rowIndex, row)
+			// log.Printf("input buffer at row: %v", (*d.inputBuffer)[rowIndex])
+			for columnIndex := range row {
+				// log.Printf("messing with coloumn %d: %v", columnIndex, currColour)
+				if d.currentBuffer.CompareXY(rowIndex, columnIndex, d.inputBuffer) {
+					// log.Printf("no update needed for %d,%d", rowIndex, columnIndex)
 				} else {
-					log.Printf("no update needed for %d,%d", x, y)
+					newColour := d.inputBuffer[rowIndex][columnIndex]
+					d.keyUpdateLimiter.Take()
+					// log.Printf("update needed for %d,%d", rowIndex, columnIndex)
+					z := keyboard.NewXYZone(uint(columnIndex)+1, uint(rowIndex))
+
+					if newColour == colour.NONE {
+						err := d.client.DeleteSignalAtZone(z)
+						if err != nil {
+							log.Printf("failed to clear signal @ %s: %v", z.GetZoneName(), err)
+						}
+					} else {
+						_, err := d.client.CreateSignal(z, keyboard.SET_COLOUR, newColour)
+						if err != nil {
+							log.Printf("failed to update signal: %v", err)
+						}
+					}
+
+					d.currentBuffer.StealXY(rowIndex, columnIndex, d.inputBuffer)
 				}
 			}
 		}
@@ -59,9 +84,14 @@ func (d *Display) Start() {
 	}
 }
 
+func (d *Display) Clear() {
+	log.Printf("clear: %s", images.CLEAR_KEYBOARD.String())
+	d.Set(images.Copy(images.CLEAR_KEYBOARD))
+}
+
 func (d *Display) listenForChange() {
 	for newInput := range d.updateChannel {
-		log.Printf("received update!")
+		log.Printf("received update! %s", newInput.String())
 		d.inputBuffer = newInput
 	}
 }
